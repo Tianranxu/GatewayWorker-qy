a54d8bb3f5e4e08cf73a12e92134c791dbaa4e84
xu <xu@gmail.com>
2018-05-11 13:46:00 +0800

    fix bug in sender for qiyu

--- Applications/wxapp/qiyuApi/sender.php
+++ Applications/wxapp/qiyuApi/sender.php
@@ -17,7 +17,7 @@ class Sender {
      *@param string $msgContent 发送请求的内容(json字符串)
      */
     public function pushMsg($msgContent){
-        $this->getUrl('https://qiyukf.com/openapi/message/send', $msgContent);
+        $url = $this->getUrl('https://qiyukf.com/openapi/message/send', $msgContent);
         return Helper::http_post($url, $msgContent);
     }
 
@@ -26,7 +26,7 @@ class Sender {
      *@param string $msgContent 发送请求的内容(json字符串)：1.uid 2. staffType:如果传0，表示机器人，传1表示人工。默认为机器人。
      */
     public function applyStaff($msgContent){
-        $this->getUrl('https://qiyukf.com/openapi/event/applyStaff', $msgContent);
+        $url = $this->getUrl('https://qiyukf.com/openapi/event/applyStaff', $msgContent);
         return Helper::http_post($url, json_encode($msgContent));
     }
 
@@ -35,7 +35,7 @@ class Sender {
      *@param string $msgContent 发送请求的内容(json字符串)
      */  
     public function updateUserInfo($msgContent){
-        $this->getUrl('https://qiyukf.com/openapi/event/updateUInfo', $msgContent);
+        $url = $this->getUrl('https://qiyukf.com/openapi/event/updateUInfo', $msgContent);
         return Helper::http_post($url, json_encode($msgContent));
     }
 
@@ -44,7 +44,7 @@ class Sender {
      *@param string $msgContent 发送请求的内容(json字符串)
      */
     public function evaluate($msgContent){
-        $this->getUrl('https://qiyukf.com/openapi/event/evaluate', $msgContent);
+        $url = $this->getUrl('https://qiyukf.com/openapi/event/evaluate', $msgContent);
         return Helper::http_post($url, json_encode($msgContent));
     }
 
@@ -53,7 +53,7 @@ class Sender {
      *@param string $msgContent 发送请求的内容(json字符串)
      */
     public function queryQueueStatus($msgContent){
-        $this->getUrl('https://qiyukf.com/openapi/event/queryQueueStatu', $msgContent);
+        $url = $this->getUrl('https://qiyukf.com/openapi/event/queryQueueStatu', $msgContent);
         return Helper::http_post($url, json_encode($msgContent));
     }
 
@@ -62,12 +62,12 @@ class Sender {
      *@param string $msgContent 发送请求的内容(json字符串)
     */
     public function quitQueue($msgContent){
-        $this->getUrl('https://qiyukf.com/openapi/event/quitQueue', $msgContent);
+        $url = $this->getUrl('https://qiyukf.com/openapi/event/quitQueue', $msgContent);
         return Helper::http_post($url, json_encode($msgContent));
     }
     
     public function getUrl($frontUrl, $msgContent){
         $time = time();
-        return $frontUrl . '?appKey='.$this->appconfig['key'].'&time='.$time.'&checksum='.Helper::getChecksum($this->appconfig['secrect'], $msgContent, $time);
+        return $frontUrl . '?appKey='.$this->appconfig['key'].'&time='.$time.'&checksum='.Helper::getChecksum($this->appconfig['secrect'], json_encode($msgContent), $time);
     }
 }
